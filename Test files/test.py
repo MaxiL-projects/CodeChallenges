61e176d26d6765b4c7ac09d895b88eb76bfd263d
Maximilian Lucassen <t-mlucassen@microsoft.com>
2020-11-24 10:58:17 +0100

    commit on main

---
+++ Test files/test.py
@@ -0,0 +1,13 @@
+dict = {
+    'spain':'madrid',
+    'portugal': 'lisbon',
+    'germany':'berlin'   
+}
+
+
+print('hi')
+
+for k,v in dict1.items() if v == 'y':
+    print('hello')
+    else: 
+    print('goodbye')
